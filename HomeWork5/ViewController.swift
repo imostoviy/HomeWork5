b3e8e0de4589616e0866cd4256adee938e904b13
imostoviy <i_mostoviy@ukr.net>
2018-11-27 21:33:06 +0200

    Initial Commit

---
+++ HomeWork5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork5
+//
+//  Created by Мостовий Ігор on 27.11.18.
+//  Copyright © 2018 Мостовий Ігор. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
